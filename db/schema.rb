7332afd2fb3f28fc62afe5882c2fc58e518dfbc2
Petr Fila <pet.fila@gmail.com>
2017-10-20 10:26:27 +1100

    sqlLite database installed

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171019121312) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "images", force: :cascade do |t|
     t.integer "property_id"
     t.datetime "created_at", null: false
@@ -49,8 +46,8 @@ ActiveRecord::Schema.define(version: 20171019121312) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
+    t.integer "current_sign_in_ip"
+    t.integer "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_sellers_on_email", unique: true
