44161edfa9dad95c333ff895f75639f91d82a8d9
Cindy Ariya <cindy.ariya@gmail.com>
2017-10-20 01:24:51 +1100

    generated devise views, contact.html.erb added, admin and contact buttons now working in header and footer. Does not include search functionality and button.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,7 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
 
+
   # GET /properties
   # GET /properties.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  resources :properties
-  resources :images
+  authenticate :seller do
+    resources :properties
+    resources :images
+  end
+
   devise_for :sellers
+
   root 'static_pages#landing'
 
-  get 'static_pages/landing'
 
   get 'static_pages/admin'
+
+  get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
