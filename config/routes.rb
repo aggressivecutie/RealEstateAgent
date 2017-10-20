b90f11882eba285f6ecc6c0e72e8753c3622eed0
Bianca Power <drbiancampower@gmail.com>
2017-10-20 12:51:57 +1100

    added form to contact page

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   root 'static_pages#landing'
 
 
-  get 'static_pages/admin'
+  get 'admin', to: 'static_pages#admin'
 
-  get 'static_pages/contact'
+  get 'contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
