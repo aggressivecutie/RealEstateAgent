e314f36a3b402827984543cdeb6bd2857d2dc23c
Cindy Ariya <cindy.ariya@gmail.com>
2017-10-20 14:18:12 +1100

    logo changed to an a tag back to landing pg.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,12 +1,8 @@
 class Image < ApplicationRecord
-<<<<<<< HEAD
   include ImageUploader::Attachment.new(:picture)
 
   belongs_to :seller
-=======
   belongs_to :properties
   include ImageUploader::Attachment.new(:picture)
   belongs_to :seller
->>>>>>> 067c13095878b56e91b0b46911e21194b1c3e52d
-
 end
