227521bf358a82fc455806636a1bc38f4a42f517
Petr Fila <pet.fila@gmail.com>
2017-10-20 16:22:34 +1100

    search by postcode works

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
-  
+
   has_many :images
+
+def self.search(search)
+  where("postcode LIKE ? OR suburb LIKE ? OR state LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+end
+
 end
